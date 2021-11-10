4c1498f3cc42c04003b64613f7e3aeabed335ebe
dev0715 <toptal126@outlook.com>
2021-11-10 12:07:01 +0300

    Remove .docusaurus cache folder and add it into .gitignore Add idea files into .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/aliraza/gitlab-repos/documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/aliraza/gitlab-repos/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/aliraza/gitlab-repos/documentation/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/aliraza/gitlab-repos/documentation/src/css/custom.css"),
-];
