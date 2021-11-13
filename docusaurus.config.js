af94311cef5609aed5b84eca607ec861d157dea2
dev0715 <toptal126@outlook.com>
2021-11-13 17:03:04 +0900

    feat: add algolia to docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,39 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Railflow Docs',
-  tagline: 'Centralized TestRail reporting for all your testing tools, frameworks, and CICD processes.',
-  url: 'https://railflow.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'railflow', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  title: "Railflow Docs",
+  tagline:
+    "Centralized TestRail reporting for all your testing tools, frameworks, and CICD processes.",
+  url: "https://railflow.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "railflow", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
 
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           //editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
         },
-       /* blog: {
+        /* blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/main/website/blog/',
         },*/
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -42,28 +43,47 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      navbar: {
+      algolia: {
+        // If Algolia did not provide you any appId, use 'BH4D9OD16A'
+        appId: "F529WOEGUL",
 
+        // Public API key: it is safe to commit it
+        apiKey: "3de8b33c9581821e84d169b67c87bb35",
+
+        indexName: "railflow",
+
+        // Optional: see doc section below
+        // contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+
+        //... other Algolia params
+      },
+      navbar: {
         logo: {
-          alt: 'Railflow Logo',
-          src: 'img/banner.png',
+          alt: "Railflow Logo",
+          src: "img/banner.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Railflow User Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Railflow User Docs",
           },
           //{to: '/blog', label: 'Blog', position: 'left'},
-         /* {
+          /* {
             href: 'https://github.com/railflow/docs',
             label: 'GitHub',
             position: 'right',
           },*/
         ],
       },
-       /** 
+      /** 
       footer: {
         style: 'dark',
         links: [
