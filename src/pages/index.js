bf5c5b393d49ca8f7b809ddf99fc5cbeabe2828f
dev0715 <toptal126@outlook.com>
2022-11-04 20:01:27 -0400

    install posthog and set env

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import  { Redirect } from 'react-router-dom';
+import posthog from 'posthog-js';
 
 export default function Home() {
+  posthog.init('phc_FtJftK2TrWqi9kbAqPeVolXtQ1FZ3JIXu7AjMPhj3Dx', { api_host: 'https://app.posthog.com' });
   return <Redirect to='/docs/intro' />;
 }
